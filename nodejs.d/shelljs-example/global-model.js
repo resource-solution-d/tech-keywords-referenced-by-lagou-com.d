5ab760b73ce1aab8b6ba3aac6495d8bdbd1a811b
resource-solution-d <resource-solution-d@repo.ren>
2017-12-14 16:30:25 +0800

    Auto-commit

---
+++ nodejs.d/shelljs-example/global-model.js
@@ -0,0 +1,22 @@
+require('shelljs/global.js');
+
+if(!which('git')) {
+    echo('Sorry, this script require git');
+    exit(1);
+}
+
+mkdir('-p', 'out/Release');
+cp('-R', 'stuff/*', 'out/Releas');
+
+cd('lib');
+ls('*.js').forEach(function (file) {
+   sed('-i', 'v0.1.2', 'v0.1.2', file);
+   sed('-i', /.*REMOVE_THIS_LINE.*\n/, '', file);
+   sed('-i', /.*REPLACE_LINE_WITH_MACRO.*\n/, cat('macro.js'));
+});
+cd('..');
+
+if(exec('git commit -am "Auto-commit"').code !== 0){
+    echo('Error: Git commit failed');
+    exit(0);
+}
